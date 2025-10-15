7207968dd5b9e992fcabd4d084b12dcf7ad37123
matias <matiasezequielmeira@gmail.com>
2025-10-15 10:01:26 -0300

    tabla proveedor

--- frozen_back/materias_primas/models.py
+++ frozen_back/materias_primas/models.py
@@ -8,6 +8,17 @@ class TipoMateriaPrima(models.Model):
     class Meta:
         db_table = "tipo_materia_prima"
 
+class Proveedor(models.Model):
+    id_proveedor = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    contacto = models.CharField(max_length=100, blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    lead_time_days = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "proveedor"
+
 class MateriaPrima(models.Model):
     id_materia_prima = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
@@ -16,6 +27,8 @@ class MateriaPrima(models.Model):
     id_tipo_materia_prima = models.ForeignKey(TipoMateriaPrima, on_delete=models.CASCADE, db_column="id_tipo_materia_prima")
     id_unidad = models.ForeignKey(Unidad, on_delete=models.CASCADE, db_column="id_unidad")
     umbral_minimo = models.IntegerField(default=0)
+    id_proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE, db_column="id_proveedor")
+    cantidad_minima_pedido = models.IntegerField(default=1)
 
     class Meta:
         db_table = "materia_prima"
