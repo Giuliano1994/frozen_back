eca1ef882a8a9a427751895102fe888fdb4c5b10
matias <matiasezequielmeira@gmail.com>
2025-10-15 10:06:47 -0300

    endpoint proveedor

--- frozen_back/materias_primas/urls.py
+++ frozen_back/materias_primas/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TipoMateriaPrimaViewSet, MateriaPrimaViewSet
+from .views import ProveedorViewSet, TipoMateriaPrimaViewSet, MateriaPrimaViewSet
 
 router = DefaultRouter()
 router.register(r'tipos', TipoMateriaPrimaViewSet)
 router.register(r'materias', MateriaPrimaViewSet)
+router.register(r'proveedores', ProveedorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
