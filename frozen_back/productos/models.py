02b186a498a7061e68014b659a40540425a00c7e
Giuliano1994 <gomesdasilva.giuliano@gmail.com>
2025-11-08 17:16:31 -0300

    creacion de combos

--- frozen_back/productos/models.py
+++ frozen_back/productos/models.py
@@ -57,3 +57,47 @@ class ImagenProducto(models.Model):
     def __str__(self):
         # Es útil para ver un nombre legible en el admin de Django
         return f"Imagen de {self.id_producto.nombre} ({self.id_imagen_producto})"
+    
+
+
+
+
+
+
+class Combo(models.Model):
+    id_combo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        db_table = "combo"
+
+    def __str__(self):
+        return self.nombre
+    
+
+class ComboProducto(models.Model):
+    id_combo_producto = models.AutoField(primary_key=True)
+
+    id_combo = models.ForeignKey(
+        Combo,
+        on_delete=models.CASCADE,
+        db_column="id_combo",
+        related_name="productos"  # combo.productos.all()
+    )
+
+    id_producto = models.ForeignKey(
+        Producto,
+        on_delete=models.CASCADE,
+        db_column="id_producto"
+    )
+
+    cantidad = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = "combo_producto"
+        unique_together = ("id_combo", "id_producto")
+
+    def __str__(self):
+        return f"{self.cantidad} x {self.id_producto.nombre} en combo {self.id_combo.nombre}"
